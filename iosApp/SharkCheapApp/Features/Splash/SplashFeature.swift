7d70b0651a0339c258e4de69169654d3a9e71f98
ByeongGwan-Seo <seo.jp.work@gmail.com>
2026-06-05 17:21:37 +0900

    feat: SplashFeature実装

--- iosApp/SharkCheapApp/Features/Splash/SplashFeature.swift
+++ iosApp/SharkCheapApp/Features/Splash/SplashFeature.swift
@@ -4,5 +4,61 @@
 //
 //  Created by Gomserker on 2026/06/01.
 //
-
+import ComposableArchitecture
 import Foundation
+
+enum SplashDisplayState: Equatable {
+  case loading
+  case loadingCompleted
+}
+
+@Reducer
+struct SplashFeature {
+  @ObservableState
+  struct State: Equatable {
+    var displayState: SplashDisplayState = .loading
+    var stores: [StoreItem] = []
+    var loadError: String?
+  }
+
+  enum Action: Equatable {
+    case onAppear
+    case storesResponse(StoresRequestResult)
+
+    enum StoresRequestResult: Equatable {
+      case success([StoreItem])
+      case failure(String)
+    }
+  }
+
+  @Dependency(\.cheapSharkClient) var cheapSharkClient
+
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .onAppear:
+        state.displayState = .loading
+        state.loadError = nil
+        return .run { send in
+          do {
+            let stores = try await cheapSharkClient.fetchStores()
+            await send(.storesResponse(.success(stores)))
+          } catch {
+            await send(.storesResponse(.failure(error.localizedDescription)))
+          }
+        }
+
+      case let .storesResponse(.success(stores)):
+        state.stores = stores
+        state.loadError = nil
+        state.displayState = .loadingCompleted
+        return .none
+
+      case let .storesResponse(.failure(message)):
+        state.loadError = message
+        state.displayState = .loadingCompleted
+        return .none
+      }
+    }
+  }
+}
